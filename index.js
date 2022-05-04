f585f9256a645d9298f2579438c5112d9ee861c7
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-04 11:48:34 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,16 @@ app.use(express());
 app.get('/', (req,res)=>{
     res.send('warehouse server running')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydash.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('db connet')
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.listen(port, ()=>{
