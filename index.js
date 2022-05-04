1939b3b3a22698db3b2b3b6ceb408342075b996a
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-04 11:37:48 +0600

    midle ware

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express());
+
 
 app.get('/', (req,res)=>{
     res.send('warehouse server running')
