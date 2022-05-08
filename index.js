d29ea5d8f9a773b843b6be393b0ad0c6f04b6c0c
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-08 08:09:37 +0600

    access token

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 app.get('/', (req,res)=>{
@@ -21,12 +22,34 @@ async function run(){
     try{
         await client.connect();
         const inventoryCollection = client.db('warehouse').collection('inventory')
+        app.post('/login', async(req,res)=>{
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN,{
+                expiresIn:'1d'
+            })
+            res.send({accessToken})
+        })
+
 
         app.get('/inventory', async(req,res)=>{
             const qurey  = {}
             const cursor =  inventoryCollection.find(qurey)
             const inventories = await cursor.toArray()
             res.send(inventories)
+        })
+        app.post('/inventory', async(req,res)=>{
+            const newInventory = req.body
+            console.log(newInventory);
+            const result = await inventoryCollection.insertOne(newInventory)
+            res.send(result)
+        })
+        app.get('/myItem', async(req,res)=>{
+            const email= req.query.email
+            console.log(email);
+            const qurey = {email: email}
+            const cursor = inventoryCollection.find(qurey)
+            const myItem = await cursor.toArray()
+            res.send(myItem)
         })
 
         app.get('/inventory/:id', async(req,res)=>{
@@ -35,6 +58,22 @@ async function run(){
             const inventory = await inventoryCollection.findOne(qurey)
             res.send(inventory)
         })
+        app.delete('/inventory/:id', async(req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await inventoryCollection.deleteOne(query)
+            res.send(result)
+        
+        })
+        app.delete('/myItem/:id', async(req,res)=>{
+            const id = req.params.id
+            console.log(id);
+            const query = {_id: ObjectId(id)}
+            const result = await inventoryCollection.deleteOne(query)
+            res.send(result)
+        
+        })
+
     }
     finally{
 
