0168716f3d6a71c3d3913578be46783250f247b0
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-08 09:19:58 +0600

    jwt verify

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJwt (req, res, next){
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    console.log('insidejwt', authHeader);
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded
+    next()
+})
+}
 
 app.get('/', (req,res)=>{
     res.send('warehouse server running')
@@ -43,13 +59,15 @@ async function run(){
             const result = await inventoryCollection.insertOne(newInventory)
             res.send(result)
         })
-        app.get('/myItem', async(req,res)=>{
-            const email= req.query.email
+        app.get('/myItem',verifyJwt, async(req,res)=>{
+            const decodedEmail = req.decoded.email
+            const email = req.query.email
+            if(email === decodedEmail)
             console.log(email);
-            const qurey = {email: email}
+           { const qurey = {email: email}
             const cursor = inventoryCollection.find(qurey)
             const myItem = await cursor.toArray()
-            res.send(myItem)
+            res.send(myItem)}
         })
 
         app.get('/inventory/:id', async(req,res)=>{
