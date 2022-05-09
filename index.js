056fe69b0ea850e8d2426b4af519e65d4f091dff
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-09 19:29:00 +0600

    put

--- index.js
+++ index.js
@@ -118,25 +118,10 @@ async function run(){
             
         
         })
-        app.put('/inventory/:id', async(req,res)=>{
-            const id = req.params.id
-            const plusQuauntity = req.body
-           console.log(plusQuauntity);
-            const query = {_id: ObjectId(id)}
-            console.log( query);
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    quantity : plusQuauntity.quantity
-                }
-            };
-    
-            const result = await inventoryCollection.updateOne(query, updatedDoc, options)
-            const resultAns = await inventoryCollection.findOne(query )
-            res.send(resultAns)
+        
             
         
-        })
+       
 
     }
     finally{
