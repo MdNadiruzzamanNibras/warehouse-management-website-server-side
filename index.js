e1b367ab4cfaa4c12fd0b1584d12edc1b808a747
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-01-05 12:34:21 +0600

    render

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ app.get('/', (req,res)=>{
 })
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydash.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydash.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run(){
     try{
